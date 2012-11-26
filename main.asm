8e80105e22e809cd9a46e797c662f38230a68646
Andrew Varnerin <ffac06@gmail.com>
2012-11-25 21:36:04 -0500

    Merged again
    Don't manually merge. the >>>>>>>HEAD and other stuff is meant for
    mergetools. Follow:
    http://max-michaels.blogspot.com/2012/02/installing-kdiff3-version-control.html
    if you want to merge 'git mergetool' is the command to run to merge

--- main.asm
+++ main.asm
@@ -17,11 +17,7 @@ Playerstructure STRUCT
 		Pname BYTE 30 dup(?)
 		Bet DWORD 0
 		Bankacc DWORD 300
-<<<<<<< HEAD
-			phand DB HAND_SIZE DUP(52)
-=======
 		phand DB HAND_SIZE DUP(52)
->>>>>>> hand value
 		CardCount BYTE 0
 Playerstructure ends
 
@@ -29,63 +25,17 @@ Playerstructure ends
 HandBuffer DB HAND_SIZE DUP(52) ;HandBuffer will hold a hand that is grabbed through a procedure.
 CardBuffer DB 52 ;CardBuffer will hold a single card
 
-<<<<<<< HEAD
-=======
+
 Dealer Playerstructure<"Dealer",0,0,HAND_SIZE DUP(52),0>
->>>>>>> hand value
+
 PlayerArray PlayerStructure 5 DUP(<>)
-
-Dealer Playerstructure<"Dealer",,,,>
-
 
 .code 
 main PROC
 	CALL randomize
 	CALL ClearRegs
-<<<<<<< HEAD
-	CALL Rungame
-	
-	
-	
-	
-	
-	;call initializedeck
-	;call shuffledeck
+	CALL RunGame
 
-	;call nameinput
-	;call outputplayers
-	;call dealcard
-	;movzx ebx, al
-	;call addcarddealer
-	;call dealcard
-	;movzx ebx, al
-	;call addcarddealer
-	;mov eax, 1
-	;call getplayerhand
-=======
-	call initializedeck
-	call shuffledeck
-	;call testDeck
-	call crlf
-	
-
-	;call nameinput
-	;call outputplayers
-	call dealcard
-	movzx ebx, al
-	call addcarddealer
-	call dealcard
-	movzx ebx, al
-	call addcarddealer
-	call dealcard
-	movzx ebx, al
-	call addcarddealer
-	call dealcard
-	movzx ebx, al
-	mov ebx, 2
-	call addcarddealer
-	
->>>>>>> hand value
 	;mov ebx, 5
 	;call getplayerhand
 	;call addcardplayer
@@ -94,7 +44,7 @@ main PROC
 	call getdealerhand
 	mov ecx, 0
 	mov cl, dealer.CardCount
-	call outputHand
+	
 	;call setplayerhand
 
 	;call getplayerhand
