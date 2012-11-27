24ef09403034da4620ab52ef64aebdde5fe4b338
Joseph Jochimsen <joejochimsen@gmail.com>
2012-11-26 22:08:44 -0500

    added dealer turn
    it's inside of joeproc.inc, just simple rule for dealer, dealer will hit
    until hand value is at least 17.  i didn't set rules for soft hits
    because it's not required and i don't think it matters what house rules
    we use

--- main.asm
+++ main.asm
@@ -12,6 +12,7 @@ INCLUDE Player.inc
 INCLUDE Hand.inc
 INCLUDE Deck.inc
 INCLUDE Game.inc
+INCLUDE joeproc.inc
 .data
 Playerstructure STRUCT
 		Pname BYTE 30 dup(?)
@@ -27,7 +28,7 @@ CardBuffer DB 52 ;CardBuffer will hold a single card
 
 
 PlayerArray PlayerStructure 5 DUP(<>)
-Dealer Playerstructure<"Dealer",0,0,HAND_SIZE DUP(52),0>
+Dealer Playerstructure<"Dealer",,,,>
 
 
 
@@ -52,28 +53,28 @@ main PROC
 	
 	
 
-	CALL CRLF
+;	CALL CRLF
 	
-	MOV ESI, OFFSET HandBuffer
-	MOV EAX, 0
-	MOV [ESI], EAX
-	MOV EAX, 13
-	MOV [ESI+1], EAX
-	MOV EAX, 34
-	MOV [ESI+2], EAX
-	MOV EAX, 51
-	MOV [ESI+3], EAX
-	MOV EAX, 52
-	MOV [ESI+4], EAX
-	MOV EAX, 0
-	CALL SetPlayerHand
-	CALL PrintHandBuffer
+;	MOV ESI, OFFSET HandBuffer
+;	MOV EAX, 0
+;	MOV [ESI], EAX
+;	MOV EAX, 13
+;	MOV [ESI+1], EAX
+;	MOV EAX, 34
+;	MOV [ESI+2], EAX
+;	MOV EAX, 51
+;	MOV [ESI+3], EAX
+;	MOV EAX, 52
+;	MOV [ESI+4], EAX
+;	MOV EAX, 0
+;	CALL SetPlayerHand
+;	CALL PrintHandBuffer
 	
-	CALL GetHandValueNew
-	CALL CRLF
-	CALL WriteInt
+;	CALL GetHandValueNew
+;	CALL CRLF
+;	CALL WriteInt
 
-	CALL CRLF
+;	CALL CRLF
 exit
 main endp
 
